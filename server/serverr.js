c8d15fbdeba2a822a6cad4ff24fde9c588eaf8a7
Nischal07bot <singhnischal953@gmail.com>
2025-07-04 20:28:41 +0530

    socket logic complete

--- server/serverr.js
+++ server/serverr.js
@@ -9,13 +9,22 @@ app.use(express.json());
 const server=createServer(app);
 const io=new Server(server);
 
-const players=[];
- 
-
+const players={};
 io.on("connection",(socket)=>{
-    console.log("a user connected");
+    console.log("a user connected",socket.id); 
+    players[socket.id]={x:0,y:0,avataridx:0};
+    socket.emit("currentplayers",players);
+    socket.broadcast.emit("currentplayer",{id:socket.id, state:players[socket.id] });
+    socket.on("move",(data)=>{
+        players[socket.id].x=data.x;
+        players[socket.id].y=data.y;
+        players[socket.id].avataridx=data.avataridx;
+        socket.broadcast.emit("playermove",{id:socket.id,state:players[socket.id]});
+    })
     socket.on("disconnect",()=>{
-        console.log("a user disconnected");
+        console.log("a user disconnected",socket.id);
+        delete players[socket.id];
+        io.emit("playerdisconnect",socket.id);
     })
 });
 const PORT=process.env.PORT || 3000;
